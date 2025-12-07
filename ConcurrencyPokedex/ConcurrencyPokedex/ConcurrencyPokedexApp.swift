bb5d8115d75b87b2bddc7e2327c5b9476f67c732
João Rouxinol <m54076@alunos.uevora.pt>
2025-12-07 14:53:02 +0000

    main

---
+++ ConcurrencyPokedex/ConcurrencyPokedex/ConcurrencyPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyPokedexApp.swift
+//  ConcurrencyPokedex
+//
+//  Created by João Rouxinol on 07/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
