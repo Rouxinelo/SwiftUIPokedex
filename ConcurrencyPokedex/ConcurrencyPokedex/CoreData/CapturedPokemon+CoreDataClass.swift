1eac2601408ad3881e8d81c8d79c5d3076f87d1f
João Rouxinol <m54076@alunos.uevora.pt>
2026-06-14 15:42:38 +0100

    fix errors in ci pipeline on coredata files

---
+++ ConcurrencyPokedex/ConcurrencyPokedex/CoreData/CapturedPokemon+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  CapturedPokemon+CoreDataClass.swift
+//  ConcurrencyPokedex
+//
+//  Created by João Rouxinol on 14/06/2026.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias CapturedPokemonCoreDataClassSet = NSSet
+
+@objc(CapturedPokemon)
+public class CapturedPokemon: NSManagedObject {
+
+}
